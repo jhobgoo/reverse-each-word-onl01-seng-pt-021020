f15eee3db8f5153fb84d473ced53b36f69a78ed4
Joel Hobgood <jhobgoo@gmail.com>
2020-02-21 01:12:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_array = []
-  string.split(" ")
-  new_array.push(string.reverse)
+  string.split = new_string
+  new_array.push(new_stirn)
   new_array.each do |string|
     puts string.reverse
   end

9d2075cf0198f8b6779452e62952620ec0c43af2
Joel Hobgood <jhobgoo@gmail.com>
2020-02-21 00:07:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
 def reverse_each_word(string)
+  new_array = []
   string = "Hello there, and how are you?"
   string.split(" ")
-  string
+    string.each do |string|
+      string += new_array
+    end
+  new_array
 end
     

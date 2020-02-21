9fbef7d846140ba994a77a0b8a8ef02a33f9b136
Joel Hobgood <jhobgoo@gmail.com>
2020-02-21 01:30:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word
   new_array = string.split
-  final_array
-  new_array.collect do |string|
+  final_array = new_array.collect do |string|
     string.reverse
   end
   new_array.join
